d0ce2fc36c3d8d323a9a2e00042f03af8e87acd3
backspace <backspace@backblog.me>
2019-03-26 16:55:19 +0800

    stage

--- test/example/middleware/mid1.js
+++ test/example/middleware/mid1.js
@@ -1,4 +1,4 @@
 // TODO: 需要支持直接的async函数
 // module.exports = async (ctx, next) => { await next()};
 
-module.exports = () => {return async (ctx, next) => { await next()}}
+module.exports = (ctx, next) => { await next()}
